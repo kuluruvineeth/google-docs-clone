ea72ee02f5cfb9b53c9cb6781c3170f5e36805a8
kuluruvineeth <kuluruvineeth8623@gmail.com>
2023-09-17 17:38:04 +0530

    - completed project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Login from "../src/pages/login";
+import Register from "../src/pages/register";
+import VerifyEmail from "../src/pages/user/verify-email";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth-context";
+import { ToastProvider } from "./contexts/toast-context";
+import { DocumentProvider } from "./contexts/document-context";
+import Document from "../src/pages/document/index";
+import AuthRoute from "./components/molecules/auth-route";
+import Create from "./pages/document/create";
+import { EditorProvider } from "./contexts/editor-context";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <ToastProvider>
+          <Routes>
+            <Route path="/" element={<h1>I am Home Page</h1>} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/user/verify-email/:token" element={<VerifyEmail />} />
+            <Route
+              path="/document/create"
+              element={<AuthRoute element={<Create />} />}
+            />
+            <Route
+              path="/document/:id"
+              element={
+                <AuthRoute
+                  element={
+                    <DocumentProvider>
+                      <EditorProvider>
+                        <Document />
+                      </EditorProvider>
+                    </DocumentProvider>
+                  }
+                />
+              }
+            />
+          </Routes>
+        </ToastProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
